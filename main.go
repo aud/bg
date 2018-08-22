1ef3578d8be1b1de696bc189a18ed37586ce09ed
Elliot Dohm <elliot.dohm@shopify.com>
2018-08-22 00:40:18 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,226 @@
+package main
+
+import (
+	"bytes"
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	_ "github.com/mattn/go-sqlite3"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"strconv"
+	"time"
+)
+
+type UnsplashLinks struct {
+	Links struct{ Download string }
+}
+
+type Headers map[string]string
+
+const (
+	accessKey           = ""
+	lastUpdatedFileName = "last_updated_time.txt"
+	imgFileName         = "main.png"
+)
+
+var (
+	getEnv  = os.Getenv
+	command = exec.Command
+)
+
+type HttpClient interface {
+	Do(req *http.Request) (*http.Response, error)
+}
+
+func get(uri string, client HttpClient, headers ...Headers) []byte {
+	req, err := http.NewRequest("GET", uri, nil)
+
+	if err != nil {
+		panic(err)
+	}
+
+	if len(headers) > 0 {
+		for key, value := range headers[0] {
+			req.Header.Set(key, value)
+		}
+	}
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return body
+}
+
+func fetchRandomImageUri(client HttpClient) string {
+	var uri = "https://api.unsplash.com/photos/random"
+
+	headers := Headers{}
+	headers["Content-Type"] = "application/json"
+	headers["Authorization"] = fmt.Sprintf("Client-ID %s", accessKey)
+
+	resp := get(uri, client, headers)
+
+	var decoded UnsplashLinks
+	json.Unmarshal(resp, &decoded)
+
+	return decoded.Links.Download
+}
+
+func fetchImage(uri string, client HttpClient) []byte {
+	return get(uri, client)
+}
+
+func updateDesktopImage() {
+	db, err := sql.Open("sqlite3", desktopDbPath())
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	_, err = db.Exec("UPDATE DATA SET VALUE = $1", homePathWithFile(imgFileName))
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func refreshDock() error {
+	_, err := command("sh", "-c", "killall Dock").Output()
+
+	return err
+}
+
+func homePath() string {
+	return getEnv("HOME")
+}
+
+func desktopDbPath() string {
+	var dbPath = "Library/Application Support/Dock/desktoppicture.db"
+
+	return fmt.Sprintf("%s/%s", homePath(), dbPath)
+}
+
+func homePathWithFile(fileName string) string {
+	home := fmt.Sprintf("%s/bg", homePath())
+
+	if _, err := os.Stat(home); os.IsNotExist(err) {
+		os.Mkdir(home, 0700)
+	}
+
+	return fmt.Sprintf("%s/%s", home, fileName)
+}
+
+func writeNewFile(contents io.Reader, path string) {
+	file, err := os.Create(path)
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	_, err = io.Copy(file, contents)
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func currentTime() int64 {
+	return time.Now().Unix()
+}
+
+func hasPastTenMinutes() bool {
+	return currentTime()-savedTime() > 30
+}
+
+func savedTime() int64 {
+	file, err := os.Open(homePathWithFile(lastUpdatedFileName))
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Bit excessive but its cheap
+	buf := make([]byte, 50)
+
+	_, err = file.Read(buf)
+
+	if err != nil {
+		panic(err)
+	}
+
+	// Strip excess bytes and convert to int64
+	trimmed := bytes.Trim(buf, "\x00")
+	i, err := strconv.ParseInt(string(trimmed), 10, 64)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return i
+}
+
+func saveCurrentTime() {
+	file, err := os.Create(homePathWithFile(lastUpdatedFileName))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer file.Close()
+
+	_, err = io.WriteString(file, fmt.Sprintf("%v", currentTime()))
+
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	var operation = func() {
+		uri := fetchRandomImageUri(http.DefaultClient)
+		img := fetchImage(uri, http.DefaultClient)
+
+		writeNewFile(
+			bytes.NewReader(img),
+			homePathWithFile(imgFileName),
+		)
+
+		updateDesktopImage()
+		saveCurrentTime()
+
+		if err := refreshDock(); err != nil {
+			fmt.Errorf("Error when trying to refresh dock: %s", err)
+		}
+	}
+
+	operation()
+
+	for {
+		if hasPastTenMinutes() {
+			operation()
+		}
+
+		time.Sleep(30 * time.Second)
+	}
+
+	panic("process killed")
+}
---
+++ main_test.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"bytes"
+	// "fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/exec"
+	"reflect"
+	"testing"
+	"time"
+)
+
+var expectedCommand string
+var expectedArgs []string
+
+type MockDefaultClient struct {
+	resp string
+}
+
+type noopIoReaderCloser struct {
+	io.Reader
+}
+
+func (noopIoReaderCloser) Close() error {
+	return nil
+}
+
+func (c *MockDefaultClient) Do(req *http.Request) (*http.Response, error) {
+	resp := &http.Response{
+		Body: noopIoReaderCloser{
+			bytes.NewBufferString(c.resp),
+		},
+	}
+
+	return resp, nil
+}
+
+func fakeExec(command string, args ...string) *exec.Cmd {
+	expectedCommand = command
+	expectedArgs = args
+
+	return &exec.Cmd{}
+}
+
+func SetGetenv(f func(key string) string) {
+	getEnv = f
+}
+
+func TestRefreshDock(t *testing.T) {
+	defer func() {
+		command = exec.Command
+	}()
+
+	command = fakeExec
+
+	refreshDock()
+
+	if "sh" != expectedCommand {
+		t.Errorf("Expected %s", expectedCommand)
+	}
+
+	if reflect.DeepEqual([]string{"-c", "killall Dock"}, expectedArgs) == false {
+		t.Errorf("Expected %s", expectedArgs)
+	}
+}
+
+func TestHomePath(t *testing.T) {
+	defer func() {
+		getEnv = os.Getenv
+	}()
+
+	var expectedValue = "value"
+
+	SetGetenv(func(key string) string {
+		switch key {
+		case "HOME":
+			return expectedValue
+		default:
+			return ""
+		}
+	})
+
+	actualValue := homePath()
+
+	if expectedValue != actualValue {
+		t.Errorf("Expected %s", actualValue)
+	}
+}
+
+func TestDesktopDbPath(t *testing.T) {
+	var expectedPath = "/Users/elliotdohm/Library/Application Support/Dock/desktoppicture.db"
+	actualPath := desktopDbPath()
+
+	if expectedPath != actualPath {
+		t.Errorf("Expected %s", actualPath)
+	}
+}
+
+func TestCurrentTime(t *testing.T) {
+	expectedTime := time.Now().Unix()
+	actualTime := currentTime()
+
+	// Handle potential race
+	if actualTime-expectedTime > 5 {
+		t.Errorf("Expected %v", actualTime)
+	}
+}
+
+func TestImgFileName(t *testing.T) {
+	if "main.png" != imgFileName {
+		t.Errorf("Expected %v", imgFileName)
+	}
+}
+
+func TestLastUpdatedFileName(t *testing.T) {
+	if "last_updated_time.txt" != lastUpdatedFileName {
+		t.Errorf("Expected %v", lastUpdatedFileName)
+	}
+}
+
+func TestGet(t *testing.T) {
+	mock := &MockDefaultClient{"data"}
+
+	resp := get("https://google.com", mock)
+
+	var expectedData = "data"
+	actualData := string(resp)
+
+	if expectedData != actualData {
+		t.Errorf("Expected %s", actualData)
+	}
+}
+
+func TestFetchRandomImageUri(t *testing.T) {
+	json := `
+		{
+			"links": {
+				"self": "https://api.unsplash.com/photos/9go97SXw30o",
+				"html": "https://unsplash.com/photos/9go97SXw30o",
+				"download": "https://unsplash.com/photos/9go97SXw30o/download",
+				"download_location": "https://api.unsplash.com/photos/9go97SXw30o/download"
+			}
+		}
+	`
+
+	mock := &MockDefaultClient{json}
+
+	resp := fetchRandomImageUri(mock)
+
+	if "https://unsplash.com/photos/9go97SXw30o/download" != resp {
+		t.Errorf("Expected %s", resp)
+	}
+}
+
+func TestFetchImage(t *testing.T) {
+	mock := &MockDefaultClient{"test"}
+
+	resp := fetchImage("https://google.com", mock)
+
+	var expectedData = "test"
+	actualData := string(resp)
+
+	if expectedData != actualData {
+		t.Errorf("Expected %s", actualData)
+	}
+}
